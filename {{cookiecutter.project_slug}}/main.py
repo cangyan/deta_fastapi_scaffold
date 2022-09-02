5ac49e70953db3094cafa8ac4ea1727fdcae0444
Mingfeng Wang <me.wmf@foxmail.com>
2022-09-02 21:26:04 +0800

    :sparkles: cors中间件

--- {{cookiecutter.project_slug}}/main.py
+++ {{cookiecutter.project_slug}}/main.py
@@ -2,13 +2,25 @@ from typing import Union
 
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.api.api_v1.api import router
 from app.config import settings
 
-app = FastAPI()
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+)
 
 app.include_router(router, prefix=settings.API_V1_STR)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def start():
