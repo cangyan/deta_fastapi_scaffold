bd375564a01455434505a43cca10bafba4093a6b
Mingfeng Wang <me.wmf@foxmail.com>
2022-09-06 14:28:20 +0800

    :heavy_plus_sign: 添加pylint

--- {{cookiecutter.project_slug}}/app/middleware/debug_api.py
+++ {{cookiecutter.project_slug}}/app/middleware/debug_api.py
@@ -1,20 +1,19 @@
 import json
 
+from app.base.logger import logger
 from fastapi.encoders import jsonable_encoder
 from starlette.concurrency import iterate_in_threadpool
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request
 from starlette.responses import Response
-from starlette.types import Message
-
-from app.base.logger import logger
+from starlette.types import ASGIApp, Message
 
 
 class DebugApiMiddleware(BaseHTTPMiddleware):
-    def __init__(self, app):
+    def __init__(self, app: ASGIApp) -> None:
         super().__init__(app)
 
-    async def set_body(self, request: Request):
+    async def set_body(self, request: Request) -> None:
         receive_ = await request._receive()
 
         async def receive() -> Message:
@@ -22,9 +21,7 @@ class DebugApiMiddleware(BaseHTTPMiddleware):
 
         request._receive = receive
 
-    async def dispatch(
-        self, request: Request, call_next: RequestResponseEndpoint
-    ) -> Response:
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
         await self.set_body(request)
         body = await request.body()
         response = await call_next(request)
