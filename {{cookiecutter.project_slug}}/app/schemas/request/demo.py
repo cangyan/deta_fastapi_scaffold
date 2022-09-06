e8d9850731d3b316b0f9460f378f4a0eaf48e5d0
Mingfeng Wang <me.wmf@foxmail.com>
2022-09-06 13:48:27 +0800

    :construction: 枚举型请求参数

--- {{cookiecutter.project_slug}}/app/schemas/request/demo.py
+++ {{cookiecutter.project_slug}}/app/schemas/request/demo.py
@@ -1,8 +1,16 @@
+from enum import Enum
 from typing import Union
 
 from pydantic import BaseModel
 
 
+class Level(str, Enum):
+    ERROR = "error"
+    WARNING = "warning"
+    INFO = "info"
+    DEBUG = "debug"
+
+
 class ReqDemoWriteLog(BaseModel):
-    level: Union[str, None] = None
+    level: Union[Level, None] = None
     message: str
